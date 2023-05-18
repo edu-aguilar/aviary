8754de175e4c667019b5e733f34727c1833dfd49
Eduardo Aguilar <eduardo.aguilar@cobee.io>
2023-05-18 17:58:17 +0200

    feat(): get birds

--- app/api/birds/route.ts
+++ app/api/birds/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import { prisma } from "@/app/lib/prismadb";
+import { NextApiRequest } from "next";
 
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
@@ -26,4 +27,34 @@ export async function POST(request: Request) {
   return new NextResponse(JSON.stringify(bird), {
     status: 201,
   });
+}
+
+export async function GET(request: NextApiRequest) {
+  const currentUser = await getCurrentUser();
+  
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const pagination = getPagination(request);
+
+  const birds = await prisma.bird.findMany({
+    skip: (pagination.pageNumber - 1) * pagination.pageSize,
+    take: pagination.pageSize,
+  });
+
+  return new NextResponse(JSON.stringify(birds), {
+    status: 200,
+  });
+}
+
+const getPagination = (request: NextApiRequest): { pageNumber: number, pageSize: number } => {
+  const { pageNumber, pageSize } = request.query;
+
+  const pagination = {
+    pageNumber: pageNumber ? (Array.isArray(pageNumber) ? parseInt(pageNumber[0]) : parseInt(pageNumber)) : 1,
+    pageSize: pageSize ? (Array.isArray(pageSize) ? parseInt(pageSize[0]) : parseInt(pageSize)) : 1,
+  }
+
+  return pagination;
 }
